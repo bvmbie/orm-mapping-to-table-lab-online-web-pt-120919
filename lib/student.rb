cbed7f467fe395af4312e0b642615f1d77842b36
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-24 16:24:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_reader :id
   attr_accessor :name, :age
+  attr_reader :id
   
   def initialize(name, age, id=nil)
     @name = name
